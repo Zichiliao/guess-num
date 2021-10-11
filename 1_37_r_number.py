df5c2bee341a2938977dfa57468c880ce24095b9
Zichi Liao <zichi710@gmail.com>
2021-10-11 10:22:35 +0800

    final version

--- 1_37_r_number.py
+++ 1_37_r_number.py
@@ -4,13 +4,17 @@
 # 猜錯的話 要告訴他 比答案大/小
 
 import random
+start = input("請決定隨機數字初始值: ")
+end = input("請決定隨機數字結束值: ")
+start = int(start)
+end = int(end)
 
-r = random.randint(1, 100)
+r = random.randint(start, end)
 count = 0
 
 while True:
 	count += 1 # count = count + 1
-	num = input('請輸入一個1~100的整數: ')
+	num = input('請輸入整數: ')
 	num = int(num)
 	if num == r:
 		print('終於猜對了!')
